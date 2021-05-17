4abaf7026a78b93791a27736ab3794822e79f0ee
DeriWars <50499055+DeriWars@users.noreply.github.com>
2021-05-17 18:40:17 +0200

    Victoire et défaite + fixes

--- Projet/Server/server.py
+++ Projet/Server/server.py
@@ -11,32 +11,36 @@ os.system("title Prodigium Server - v2021.05.15 - by DeriWars (113 block)")
 
 class Battle():
     def __init__(self, player, addr, name):
-        self.player_1 = player
-        self.player_1_name = name
-        self.player_1_addr = addr
-        self.player_1_attack = None
-        self.player_1_team = None
+        self.player_1 = {
+            "conn": player,
+            "name": name,
+            "addr": addr,
+            "attack": None,
+            "team": None,
+            "statement": None
+        }
         
-        self.player_2 = None
-        self.player_2_name = None
-        self.player_2_addr = None
-        self.player_2_attack = None
-        self.player_2_team = None
+        self.player_2 = {
+            "conn": None,
+            "name": None,
+            "addr": None,
+            "attack": None,
+            "team": None,
+            "statement": None
+        }
         
-        self.turn = 0
         self.is_accepted = False
+        self.is_finished = False
         
     def accept(self, player, addr, name):
-        self.player_2 = player
-        self.player_2_name = name
-        self.player_2_addr = addr
+        self.player_2.update({"conn": player, "addr": addr, "name": name})
         self.is_accepted = True
     
     def broadcast(self, msg):
-        self.send(self.player_1, msg)
+        self.send(self.player_1.get("conn"), msg)
 
-        if self.player_2 != None:
-            self.send(self.player_2, msg)
+        if self.player_2.get("conn") != None:
+            self.send(self.player_2.get("conn"), msg)
     
     def send(self, conn, msg):
         msg = msg.encode("UTF-8")
@@ -44,49 +48,73 @@ class Battle():
         conn.send(msg)
         
     def set_team(self, name, team):
-        if name == self.player_1_name:
-            self.player_1_team = team
-        elif name == self.player_2_name:
-            self.player_2_team = team
+        if name == self.player_1.get('name'):
+            self.player_1.update({"team": team})
+        elif name == self.player_2.get('name'):
+            self.player_2.update({"team": team})
         
-        if self.player_1_team != None and self.player_2_team != None:
-            self.send(self.player_1, self.player_2_team)
-            self.send(self.player_2, self.player_1_team)
-            print(f"[{Color.CVIOLET}/{Color.CEND}] ({self.__repr__()}) Teams have been sended to every players involved.")
+        if self.player_1.get("team") != None and self.player_2.get("team") != None:
+            self.send(self.player_1.get("conn"), self.player_2.get("team"))
+            self.send(self.player_2.get("conn"), self.player_1.get("team"))
+            print(f"[{Color.CVIOLET}/{Color.CEND}] ({self.__repr__()}) Teams have been sended to {self.player_1.get('name')} and {self.player_2.get('name')}.")
             
 #region Attack
 
     def set_player_attack(self, name, index):
-        if name == self.player_1_name:
+        if name == self.player_1.get('name'):
             self.set_player_1_attack(index)
-        elif name == self.player_2_name:
+        elif name == self.player_2.get('name'):
             self.set_player_2_attack(index)
 
     def set_player_1_attack(self, index):
-        self.player_1_attack = index
+        self.player_1.update({"attack": index})
         
-        if self.player_2_attack != None:
+        if self.player_2.get('attack') != None:
             self.proceed()
             
     def set_player_2_attack(self, index):
-        self.player_2_attack = index
+        self.player_2.update({"attack": index})
 
-        if self.player_1_attack != None:
+        if self.player_1.get('attack') != None:
             self.proceed()
 
     def proceed(self):
-        self.send(self.player_1, f"{self.player_2_attack}")
-        self.player_2_attack = None
+        self.send(self.player_1.get('conn'), f"{self.player_2.get('attack')}")
+        self.player_2.update({"attack": None})
         
-        self.send(self.player_2, f"{self.player_1_attack}")
-        self.player_1_attack = None
-        
-        self.turn += 1
+        self.send(self.player_2.get('conn'), f"{self.player_1.get('attack')}")
+        self.player_1.update({"attack": None})
 
 #endregion
-        
+    
+    def set_statement(self, name, statement):
+        if name == self.player_1.get("name"):
+            self.player_1.update({"statement": statement})
+            print(f"[{Color.CVIOLET}/{Color.CEND}] ({self.__repr__()}) {name} has sent his statement.")
+        elif name == self.player_2.get("name"):
+            self.player_2.update({"statement": statement})
+            print(f"[{Color.CVIOLET}/{Color.CEND}] ({self.__repr__()}) {name} has sent his statement.")
+            
+        if self.player_1.get("statement") != None and self.player_2.get("statement") != None:
+            if self.player_1.get("statement") == self.player_2.get("statement"):
+                self.broadcast("draw")
+                print(f"[{Color.CVIOLET}/{Color.CEND}] ({self.__repr__()}) DRAW")
+            elif self.player_1.get("statement") == "win":
+                self.send(self.player_1.get("conn"), "win")
+                self.send(self.player_2.get("conn"), "loose")
+                print(f"[{Color.CVIOLET}/{Color.CEND}] ({self.__repr__()}) {self.player_1.get('name')} WIN")
+            elif self.player_2.get("statement") == "win":
+                self.send(self.player_2.get("conn"), "win")
+                self.send(self.player_1.get("conn"), "loose")
+                print(f"[{Color.CVIOLET}/{Color.CEND}] ({self.__repr__()}) {self.player_2.get('name')} WIN")
+            else:
+                self.broadcast("draw")
+                print(f"[{Color.CVIOLET}/{Color.CEND}] ({self.__repr__()}) Result unknown, declaring a draw...")
+            
+            self.is_finished = True
+    
     def __repr__(self):
-        return f"[{self.player_1_addr[0]}] {self.player_1_name} // [{self.player_2_addr[0]}] {self.player_2_name}"
+        return f"{self.player_1.get('name')} VS {self.player_2.get('name')}"
 
 
 
@@ -123,24 +151,36 @@ class Server():
             while True:
                 msg: str = self.receive(conn)
                 
+                if battle != None:
+                    if battle.is_finished:
+                        battle = None
+                        fighting = False
+                
                 if msg != None and msg != "":
                     if msg == "server-disconnect": # USED TO DISCONNECT THE CLIENT
                         break
                     elif msg.startswith("match"): # USED TO CREATE OR ACCEPT A MATCH
                         if not fighting:
                             battle = self.match(conn, addr, name, msg.replace("match ", ""))
+                            
+                            if battle != None:
+                                fighting = True
                         else:
                             print(f"[{Color.CVIOLET}/{Color.CEND}] Can't create another match while your are fighting")
                             self.send(conn, "impossible")
                     elif msg.startswith("attack") and battle != None: # USED TO ATTACK DURING A BATTLE
-                        index = int(msg.replace("attack ", ""))
+                        index = msg.replace("attack ", "")
                         battle.set_player_attack(name, index)
                     elif msg == "decline": # USED TO DECLINE A MATCH
                         self.decline(name)
                     elif msg.startswith("team ") and battle != None: # USED TO SYNC TEAM BETWEEN PLAYERS
                         battle.set_team(name, msg.replace("team ", ""))
-                    elif msg == "death": # USED TO END THE BATTLE
-                        pass
+                    elif msg == "win": # USED TO END THE BATTLE (WIN)
+                        battle.set_statement(name, "win")
+                        self.clear(name, "")
+                    elif msg == "loose":  # USED TO END THE BATTLE (LOOSE)
+                        battle.set_statement(name, "loose")
+                        self.clear(name, "")
                     elif msg == "list": # USED TO DISPLAY THE LIST OF PLAYER AVAILABLE
                         self.send(conn, ";".join(self.alive))
                     else:
@@ -149,10 +189,12 @@ class Server():
         except Exception as e:
             if not type(e) is ConnectionResetError:
                 print(f"{Color.CRED2}[E] Client {name} ({addr[0]}) has encountered an issue. [{str(type(e))}]{Color.CEND}")
+                print(e.with_traceback())
                     
         
         if name in self.alive:
             self.alive.remove(name)
+            self.clear(name, "", True)
         
         print(f"[{Color.CRED2}-{Color.CEND}] Client {name} ({addr[0]}) has been disconnected.")
         conn.close()
@@ -160,7 +202,7 @@ class Server():
     def check_opponent(self, opponent):
         for battle in self.battles:
             if battle.is_accepted:
-                if battle.player_1_name == opponent or battle.player_2_name == opponent:
+                if battle.player_1.get('name') == opponent or battle.player_2.get('name') == opponent:
                     return True
                 
         return False
@@ -196,31 +238,32 @@ class Server():
             print(f"[{Color.CVIOLET}/{Color.CEND}] Creating a match is only available with another person than the challenger.")
             
         for battle in self.battles:
-            if battle.player_1_name == opponent:
+            if battle.player_1.get('name') == opponent:
                 if not battle.is_accepted:
                     battle.accept(player, addr, name)
-                    print(f"[{Color.CVIOLET}/{Color.CEND}] Match accepted between [{battle.player_1_addr[0]}] {battle.player_1_name} and [{battle.player_2_addr[0]}] {battle.player_2_name}")
+                    print(f"[{Color.CVIOLET}/{Color.CEND}] Match accepted between {battle.player_1.get('name')} and {battle.player_2.get('name')}")
                     
                 return battle
         
         new_battle = Battle(player, addr, name)
         self.battles.append(new_battle)
-        print(f"[{Color.CVIOLET}/{Color.CEND}] Match created by [{new_battle.player_1_addr[0]}] {new_battle.player_1_name}")
+        print(f"[{Color.CVIOLET}/{Color.CEND}] Match created by {new_battle.player_1.get('name')}")
         return self.battles[len(self.battles) - 1]
         
-    def clear(self, player_1, player_2):
+    def clear(self, player_1, player_2, ignore=False):
         new_battles = []
         
         for battle in self.battles:
-            if battle.is_accepted:
+            if (battle.is_accepted and not battle.is_finished) and not ignore:
                 new_battles.append(battle)
                 continue
             
-            if not (battle.player_1_name == player_1 or battle.player_2_name == player_1 or battle.player_1_name == player_2 or battle.player_2_name == player_2):
+            if not (battle.player_1.get("name") == player_1 or battle.player_2.get("name") == player_1 or battle.player_1.get("name") == player_2 or battle.player_2.get("name") == player_2):
                 new_battles.append(battle)
                 continue
             
-            battle.broadcast("decline")
+            if not ignore:
+                battle.broadcast("decline")
         
         self.battles = new_battles
         
@@ -232,7 +275,7 @@ class Server():
                 new_battles.append(battle)
                 continue
 
-            if not (battle.player_1_name == name or battle.player_2_name == name):
+            if not (battle.player_1.get("name") == name or battle.player_2.get("name") == name):
                 new_battles.append(battle)
                 continue
             
